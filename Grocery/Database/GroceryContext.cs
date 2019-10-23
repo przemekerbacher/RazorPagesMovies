87c046c6e3880b73b12e5815100536694c1580db
Przemyslaw Erbacher <przemyslaw.erbacher@gmail.com>
2019-10-23 18:06:33 +0200

    Dodaj pliki projektów.

---
+++ Grocery/Database/GroceryContext.cs
@@ -0,0 +1,15 @@
+﻿using Grocery.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Grocery.Database
+{
+    public class GroceryContext : DbContext
+    {
+        public GroceryContext(DbContextOptions<GroceryContext> options)
+           : base(options)
+        {
+
+        }
+        public DbSet<Product> Products { get; set; }
+    }
+}
